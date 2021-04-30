498d3fab2ead059d887b57b8b78151701c4f3cfc
Michael DoHo <michaeldoho@hotmail.com>
2021-04-30 16:47:55 -0400

    updated fetch call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Game from './components/Game.js';
 import Start from "./components/Start.js";
+import React, { useState, useEffect } from 'react';
 
 
 const App = () => {
+    
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean')
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+});
+
     return (
         <Router>
            <Switch>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,11 @@
 // import styled from "styled-components";
 
 
+
+
+
 const Game = () => {
 
-    const fetchItems = async () => {
-        const data = await fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean');
-        const items = await data.json();
-        console.log(items);
-    };
-    fetchItems();
     return (
         <div>
             question 1
