3075ec93953091f87d2b743345a7da61252104e6
Michael DoHo <michaeldoho@hotmail.com>
2021-05-01 17:24:59 -0400

    wrote javascript that makes question 1, true answer, and false answer appear on game page

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,5 @@
 // import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
 
 
 
@@ -6,9 +7,28 @@
 
 const Game = () => {
 
+    const [questions, setQuestions] = useState(false);
+    const [questionNumber, setquestionNumber] = useState(0)
+
+    useEffect(() => {
+        if (!questions) {
+            fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean')
+            .then((response) => response.json())
+            .then((data) => setQuestions(data));
+        }
+}//ask erik about how to write log catch errors with this^
+    );
+
     return (
         <div>
-            question 1
+                                         {/* why does ': ("")' need to be here---->\/ */}
+           {questions ? ( <p>{JSON.stringify(questions.results[0].question)}</p>) : ("")}
+           {/*        ^ <---ask erik why removing this 'question mark' gives an error */}
+
+            {questions ? ( <button>{JSON.stringify(questions.results[0].incorrect_answers)}</button>) : ("")}
+            
+            {questions ? ( <button>{JSON.stringify(questions.results[0].correct_answer)}</button>) : ("")}
+
         </div>
     )
 }
