592e5e58ac3b507f064aa844833be42a46249dd6
Michael DoHo <michaeldoho@hotmail.com>
2021-07-06 17:39:18 -0400

    trying to add end game score page

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,6 @@
 // import styled from "styled-components";
 import React, { useState, useEffect } from 'react';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Trivia from "./Trivia";
 
 const API_URL = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple';
@@ -28,14 +28,24 @@ const Game = () => {
         }
     };
 
-    return question.length > 0 ? (
+    if (currentIndex < 9) {
+    return (
         <div>
             <Trivia data={question[currentIndex]} handleAnswer={handleAnswer} />
             <div>Score: {score}</div>
-        </div>
-    ) : (
-        <h1>loading</h1>
-    );
+            </div>
+    )
+    } else {
+        return (
+            <div>
+                <p>Done! Your score is</p>
+                <div>{score}</div>
+                <Link to={'/'}>
+                    <button>Try again</button>
+                </Link>
+            </div>
+        )
+    }
 }
 
 export default Game
